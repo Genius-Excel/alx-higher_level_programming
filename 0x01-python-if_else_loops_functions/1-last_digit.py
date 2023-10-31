8691a44b66608db3527279ed1ccb325ea88141a3
Genius-Excel <geniusexcel9@gmail.com>
2023-10-31 04:40:20 -0700

    Resolved to suite pycodestyle.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,8 @@ number = random.randint(-10000, 10000)
 last_int = number % 10
 
 if last_int < 6 and last_int != 0:
-#    negated_int = -(last_int)
-#    if number < 0:
-#        print("Last digit of {number} is -{last_int} and is less than 6 and not 0")
     print(f"Last digit of {number} is {last_int} and is less than 6 and not 0")
 elif last_int == 0:
     print(f"Last digit of {number} is {last_int} and is 0")
 elif last_int > 5:
-   print(f"Last digit of {number} is {last_int} and is greater than 5")
+    print(f"Last digit of {number} is {last_int} and is greater than 5")
