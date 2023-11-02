7af683d21a5e6ec8d9204031ec2a942e578b11ae
Genius-Excel <geniusexcel9@gmail.com>
2023-11-02 12:11:53 -0700

    Args count and argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arg_count = len(sys.argv)
+
+    arg_list = list(sys.argv)
+
+    if arg_count == 1:
+        print("0 arguments")
+
+    if arg_count == 2:
+        print("{} argument:\n{}: {}".format(1, 1, arg_list[1]))
+
+    if arg_count > 2:
+        print("{} arguments:".format(arg_count - 1))  # excluding filename
+
+        for arg in range(1, arg_count):
+
+            print("{}: {}".format(arg, arg_list[arg]))
