906e4af085a77290047e05c06fb24d58b0a6b692
Genius-Excel <geniusexcel9@gmail.com>
2023-11-02 10:25:17 -0700

    This file is module where the function 'add' is defined.

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add(a, b):
+    """This function adds 2 numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The value of a + b.
+    """
+
+#    global a
+#    global b
+    a = 1
+    b = 2
+    return (a + b)
+
+#print("{} + {} = {}".format(a, b, add(a, b)))
