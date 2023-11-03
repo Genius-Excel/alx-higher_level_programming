b69a4a2cc1057fc22d88ee1d7d5aaa02cfd1eda4
Genius-Excel <geniusexcel9@gmail.com>
2023-11-02 18:11:44 -0700

    Using the 'dir' function to print the names in a module.

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    name_list = dir(hidden_4)
+
+    for name in range(len(name_list)):
+        if not name_list[name].startswith('__'):
+            print(name_list[name])
