0633c7b4d37a5527b44abe55c01dc350aea9143d
Genius-Excel <geniusexcel9@gmail.com>
2023-11-05 11:49:49 -0800

    Replaces an element in a list.

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0:
+    if idx > len(my_list) or idx < 0:
         return my_list
-
-    if idx > len(my_list):
-        return my_list
-
-    my_list[idx] = element
+    else:
+        my_list[idx] = element
 
     return my_list
