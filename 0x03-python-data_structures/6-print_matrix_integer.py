0d6fb166b8ab4f86eb7a174f200f00aa11adf43a
Genius-Excel <geniusexcel9@gmail.com>
2023-11-06 09:44:20 -0800

    Prints the integers of a matrix.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for item_a in matrix:
         for item_b in item_a:
-            print("{}".format(item_b), end=' ')
+            print("{:d}".format(item_b), end=' ')
         print()
