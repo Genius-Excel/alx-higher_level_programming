f8b006869f5b95a6ac4c9a6098e280b01025c1e4
Genius-Excel <geniusexcel9@gmail.com>
2023-11-06 11:37:23 -0800

    modified.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    sent_len = len(sentence)
-
-    fisrt_char = None
+    # sent_len = len(sentence)
 
     if not sentence:
-        pass
+        fisrt_char = None
     else:
         first_char = sentence[0]
 
-    return (sent_len, first_char)
+    return (len(sentence), first_char)
