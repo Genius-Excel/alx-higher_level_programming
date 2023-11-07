aa73f16039b0e744172ef2c3c69af74123f6e215
Genius-Excel <geniusexcel9@gmail.com>
2023-11-06 16:12:08 -0800

    Remove element at specific index.

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    list_len = len(my_list)
+
+    copied_list = my_list.copy()
+
+    if idx >= list_len or idx < 0:
+        return (my_list)
+
+    else:
+        my_list.remove(idx)
+
+    return (my_list)
