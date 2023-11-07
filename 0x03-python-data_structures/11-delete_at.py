efd4100ec24f891e0937d7045833e337eefddae1
Genius-Excel <geniusexcel9@gmail.com>
2023-11-06 16:37:27 -0800

    Delete an element at a index.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if my_list:
+    list_len = len(my_list)
 
-        list_len = len(my_list)
+    if idx >= list_len or idx < 0:
+        return (my_list)
 
-        if idx >= list_len or idx < 0:
-            return (my_list)
-
-        else:
-            my_list.remove(idx)
-            return my_list
+    else:
+        my_list.remove(idx)
+        return my_list
