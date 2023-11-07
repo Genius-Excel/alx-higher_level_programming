13adeba9634ca1d6f7ded0883c08ce6a071fb238
Genius-Excel <geniusexcel9@gmail.com>
2023-11-06 16:31:53 -0800

    Remove an element.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    list_len = len(my_list)
+    if my_list:
 
-    copied_list = my_list.copy()
+        list_len = len(my_list)
 
-    if idx >= list_len or idx < 0:
-        return (my_list)
+        if idx >= list_len or idx < 0:
+            return (my_list)
 
-    else:
-        my_list.remove(idx)
-        return my_list
+        else:
+            my_list.remove(idx)
+            return my_list
