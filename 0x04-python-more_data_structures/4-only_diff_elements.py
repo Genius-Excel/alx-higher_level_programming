bc85f2c0eb8146d9e2b2ef60ebfa8e25cd59cd2c
Genius-Excel <geniusexcel9@gmail.com>
2023-11-08 08:41:18 -0800

    Only difference.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,15 +4,13 @@ def only_diff_elements(set_1, set_2):
 
     unique_list = []
 
-    if set_1 and set_2:
+    for item_1 in set_1:
+        for item_2 in set_2:
+            if item_1 not in set_2:
+                unique_list.append(item_1)
+            if item_2 not in set_1:
+                unique_list.append(item_2)
 
-        for item_1 in set_1:
-            for item_2 in set_2:
-                if item_1 not in set_2:
-                    unique_list.append(item_1)
-                if item_2 not in set_1:
-                    unique_list.append(item_2)
-
-        comp_set = set(unique_list)
+    comp_set = set(unique_list)
 
     return (comp_set)
