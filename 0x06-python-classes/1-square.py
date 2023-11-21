521ee910f7d84fa97c72bd0871189d05d739a3ca
Genius-Excel <geniusexcel9@gmail.com>
2023-11-21 12:37:51 -0800

    Modified to pass requirements.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,16 @@
 
 "This is a class definition for a Square with a private variable."
 
+
 class Square:
     """This class is a boilerplate for the square
     artihmetics. The size attribute is made private.
 
+    Args:
+        __size (int): private attribute of the Sqaure class.
+
     Attributes:
-        size (int): side of the sqaure integer.
+        __size (int): side of the sqaure integer.
     """
     def __init__(self, __size):
         self.__size = __size
