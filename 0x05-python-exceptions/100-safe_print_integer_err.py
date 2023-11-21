cf27ef5109a9894d6be505a55d9684cda6a4765b
Genius-Excel <geniusexcel9@gmail.com>
2023-11-20 17:19:39 -0800

    Modified to meet requirement.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,9 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-#        isinstance(value, int)
+        isinstance(value, int)
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        print("{}".format(e), file=sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
