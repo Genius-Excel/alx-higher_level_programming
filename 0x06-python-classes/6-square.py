18b0c1666372cc354c8441514c588d80bb52042c
Genius-Excel <geniusexcel9@gmail.com>
2023-11-21 15:14:52 -0800

    Requirements met.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,7 +16,7 @@ class Square:
                     of the square with the int datatype.
     """
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
