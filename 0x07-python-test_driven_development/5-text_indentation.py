93ffef8e72c0033fa74c0b947121ec4ec51fe2ba
Genius-Excel <geniusexcel9@gmail.com>
2023-11-28 07:03:32 -0800

    This module performs text indentations.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""This module contains a function that does a text indentation.
+
+   Args:
+       text (str): this is the string represntation for which
+       indentation is to be done.
+"""
+
+
+def text_indentation(text):
+    """This function indents the text string based on the delimeters.
+       Returns: None
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for ch in text:
+        if ch == '.' or ch == ':' or ch == '?':
+            print("{}".format(ch), end='')
+            print("\n")
+        else:
+            print("{}".format(ch), end='')
