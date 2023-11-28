c41623b6e56ae27e42fcf5bc453172886b702362
Genius-Excel <geniusexcel9@gmail.com>
2023-11-28 03:32:35 -0800

    Print user first name and last name.

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""This module contains a function tha prints the name of user.
+   Args:
+       first_name (str): this reperesents the first name of the user
+       last_name (str): this represents the last name of the user.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
