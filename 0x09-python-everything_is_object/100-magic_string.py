da075201a42168d7ff0cb0a280105ce7d5d96a8c
Genius-Excel <geniusexcel9@gmail.com>
2023-11-29 06:22:00 -0800

    Magic string.

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return f"Bestschool{' ,Bestschool' * (magic_string.count -1)}"
+    return f"BestSchool{' ,BestSchool' * (magic_string.count -1)}"
