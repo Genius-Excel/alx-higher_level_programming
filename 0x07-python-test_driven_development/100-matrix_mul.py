8e90a1d8bd04146b48f82b78b4ca83a12d2e801c
Genius-Excel <geniusexcel9@gmail.com>
2023-11-30 16:52:37 +0100

    File made compliant with pycodestyle.

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -30,7 +30,7 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    def valid_matrix_lists(mat :list[list]) -> bool:
+    def valid_matrix_lists(mat: list[list]) -> bool:
         """This function validates the object of the matrix list.
            it checks the sub items of the matrix to check if it's a list.
 
@@ -46,9 +46,9 @@ def matrix_mul(m_a, m_b):
         for sub_list in mat:
             if not isinstance(sub_list, list):
                 return False
-            
+
         return True
-            
+
     if not valid_matrix_lists(m_a):
         raise TypeError("m_a must be a list of lists")
 
@@ -77,22 +77,25 @@ def matrix_mul(m_a, m_b):
 
     # Try except a ValueError occured during multiplication.
     try:
+        err_ma = "m_a should contain only integers or floats"
+
+        err_mb = "m_b should contain only integers or floats"
         for i in range(mat_a_size):
             new_row = []
             for j in range(mat_b_col_size):
                 element_prod = 0
                 for k in range(mat_b_size):
                     if not isinstance(m_a[i][k], (int, float)):
-                        raise TypeError("m_a should contain only integers or floats")
+                        raise TypeError(err_ma)
 
                     if not isinstance(m_b[k][j], (int, float)):
-                        raise TypeError("m_b should contain only integers or floats")
+                        raise TypeError(err_mb)
                     element_prod += m_a[i][k] * m_b[k][j]
                 new_row.append(element_prod)
 
             new_matrix_list.append(new_row)
-    except:
-        ValueError("m_a and m_b can't be multiplied")
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
     # print(f"mat a size: {mat_a_size}")
     # print(f"mat b size: {mat_b_size}")
     # print(f"mat b col size: {mat_b_col_size}")
@@ -105,7 +108,7 @@ def matrix_mul(m_a, m_b):
 matrix1 = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    (7, 8, 9)
 ]
 
 matrix2 = [
