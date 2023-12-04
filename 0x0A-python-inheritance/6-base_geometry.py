6a3819c99136c81804d8df5b3f535c6e7ed65102
Genius-Excel <geniusexcel9@gmail.com>
2023-12-04 09:25:52 -0800

    This module contains a class th raises n exception.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class that raises an exception."""
+
+
+class BaseGeometry:
+    """This clss raises an exception"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
