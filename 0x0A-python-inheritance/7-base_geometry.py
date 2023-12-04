8bb9b738ffa7ebbb7c1e4260c2b3399915ea1b70
Genius-Excel <geniusexcel9@gmail.com>
2023-12-04 09:39:35 -0800

    This module raises an exception for area and integer validator.

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class that raises an exception.
+   and validates an integer
+"""
+
+
+class BaseGeometry:
+    """This calss raises an exception and validated an integer"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
