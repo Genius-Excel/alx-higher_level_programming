3e2354c1fafef708598574126d32a13df9e02a8a
Genius-Excel <geniusexcel9@gmail.com>
2023-12-04 15:57:44 -0800

    This module contains a class that inherits from BaseGeometry class.

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class that raises an exception.
+   and validates an integer
+"""
+
+
+class BaseGeometry:
+    """This calss raises an exception and validated an integer"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+        if not isinstance(name, str):
+            raise TypeError("{} must be a string".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """This class is a defintion of a rectangle class which inherits
+       methods of the BaseGeometry class.
+    """
+
+    def __init__(self, width, height):
+        # super().__init__()
+        self.integer_validator("width", width)
+
+        self.integer_validator("height", height)
+
+        self.__height = height
+
+        self.__width = width
