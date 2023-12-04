14ca45d688660120953583161cf472559ea0db93
Genius-Excel <geniusexcel9@gmail.com>
2023-12-04 07:16:37 -0800

    This file contains a function that prints a list in sorted order. but yet to be fixed.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""This module cntains a class that inherits from list class """
+
+
+class MyList(list):
+    """ This class contains the implementation of sorting a list."""
+
+    def print_sorted(self):
+        self.sorted_list = []
+
+        self.sorted_list = sorted(list)
+
+        print(self.sorted_list)
