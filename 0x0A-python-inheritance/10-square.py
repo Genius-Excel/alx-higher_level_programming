1e26e5daeb90f391948b7760e0eb2725dd15ae64
Genius-Excel <geniusexcel9@gmail.com>
2023-12-04 16:21:58 -0800

    This module classes that overrides the methods of it's base class.

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class that raises an exception.
+   and validates an integer
+"""
+
+
+class BaseGeometry:
+    """This calss raises an exception and validated an integer"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+        if not isinstance(name, str):
+            raise TypeError("{} must be a string".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """This class is a defintion of a rectangle class which inherits
+       methods of the BaseGeometry class.
+    """
+
+    def __init__(self, width, height):
+        # super().__init__()
+        self.integer_validator("width", width)
+
+        self.integer_validator("height", height)
+
+        self.__height = height
+
+        self.__width = width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return (self.__width * self.__height)
+
+
+class Square(Rectangle):
+    """This class is a definiton of a square class which inherits
+       methods of the Rectangle class.
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+    def area(self):
+        return (self.__size ** 2)
