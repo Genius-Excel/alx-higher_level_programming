772d4deaab8a6e836b198b56eaafd9eb58658015
Genius-Excel <geniusexcel9@gmail.com>
2023-12-06 10:52:38 +0100

    This module contains a funtion that serializes a JSON object and saves it to a file.

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 """this module contains a function that writes a serialized
-   JSON object to a file. 
+   JSON object to a file.
 """
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """This function writes and saves a JSON object to a file.
+       Args:
+           my_obj (any): JSON object to be serialized.
+           filename (any): This represents the file to be written to.
+    """
     with open(filename, "w") as the_file:
         return the_file.write(json.dumps(my_obj))
