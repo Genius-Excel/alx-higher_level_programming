10ebab0369450427bdff49a47d15269b462b1e8c
Genius-Excel <geniusexcel9@gmail.com>
2023-12-06 17:38:47 +0100

    This module contains a function that search for a string and appends a new string to it. --Function documentations added.

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,6 +6,15 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """This function searches for a specific string and appends
+       a new string to file.
+       Args:
+           filename (any): this represents the filename.
+           search_string (any): this represents the string to be located.
+           new_string (any): this represents the new string to be appended.
+       Returns:
+           None
+    """
     # open the file for read mode if it exists.
     with open(filename, "r") as my_file:
         read_lines = my_file.readlines()
