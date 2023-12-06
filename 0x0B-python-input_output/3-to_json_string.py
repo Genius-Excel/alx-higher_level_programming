501d0ce2c34f563676ab09d5ecc536dcc249ac84
Genius-Excel <geniusexcel9@gmail.com>
2023-12-06 10:06:03 +0100

    This module contains a function that returns a serialized object.

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module contains a function that serializes an object with JSON"""
+
+import json
+
+def to_json_string(object):
+    """This function serializes an object to a JSON string
+       Args:
+           object (any): Object to be serialized.
+       Returns:
+           JSON object that has been serialized.    
+    """
+
+    return (json.dumps(object))
