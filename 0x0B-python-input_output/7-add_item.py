655b55b97a3b85e480fa4dfcca70990a37dc5989
Genius-Excel <geniusexcel9@gmail.com>
2023-12-06 14:10:29 +0100

    File modified.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,35 @@
    arguments to a JSON object file.
 """
 
+import json
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+# save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+# load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def save_to_json_file(my_obj, filename):
+    """This function writes and saves a JSON object to a file.
+       Args:
+           my_obj (any): JSON object to be serialized.
+           filename (any): This represents the file to be written to.
+    """
+    with open(filename, "w") as the_file:
+        return the_file.write(json.dumps(my_obj))
+
+
+def load_from_json_file(filename):
+    """This creates an object from a JSON file and returns the object
+       Args:
+           filename (any): this represents the JSON file to be read
+       Returns:
+           The JSON object read from the file.
+    """
+
+    with open(filename, "r") as the_file:
+        json_object = json.load(the_file)
+
+        return json_object
 
 
 def add_arguments(json_file, *args):
