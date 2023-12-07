4e0c906f76af5215e866a47ae5937ccf91928835
Genius-Excel <geniusexcel9@gmail.com>
2023-12-07 21:09:28 +0100

    This module contains a class defintion for the Base class.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,18 @@
 """This module contains a class defintion of the Base models class"""
 
 
-class Base:
+class Base(object):
     """This is the class defition of the base model.
 
        Attributes:
            id (int): this represents the id of the class.
     """
+
+    __nb_object = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_object += 1
+            self.id = Base.__nb_object
