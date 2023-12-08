c3c962395386695ebb13b6e380993686c9a3b896
Genius-Excel <geniusexcel9@gmail.com>
2023-12-08 16:09:41 +0100

    Validation of the class attributes was done.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,6 +39,30 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id=id)
+        # validate the width attribute.
+        if not isinstance(width, int):
+            raise TypeError("width must be an intger")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        
+        # validate the height attribute
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        
+        # validate x-cordinate attribute
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        
+        # validae the y-cordinate attribute
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        
         self.__width = width
         self.__height = height
         self.__x = x
