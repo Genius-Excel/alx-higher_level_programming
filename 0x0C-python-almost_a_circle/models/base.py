0686a23bc08f4b1ea933a45bc7afddb364bccf87
Genius-Excel <geniusexcel9@gmail.com>
2023-12-09 21:46:53 +0100

    File module to be updated.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 """This module contains a class defintion of the Base models class"""
 
 
+import json
+
+
 class Base(object):
     """This is the class defition of the base model.
 
@@ -18,3 +21,18 @@ class Base(object):
         else:
             Base.__nb_object += 1
             self.id = Base.__nb_object
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        for obj in list_objs:
+            cls.filename = "{}.json".format(obj.__class__.__name__)
+
+        with open(cls.filename, "w") as file:
+            file.write(cls.to_json_string(list_objs))
