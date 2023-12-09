0013b1874c3869f632a04407edc359bf9a2d5a1a
Genius-Excel <geniusexcel9@gmail.com>
2023-12-09 07:27:36 -0800

    This module contains a Square class the extends he Rectangle class.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This module contains a class Square that extends the Rectngle class"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class is defined for the Square clss that extends
+       the Rectangle class.
+       Atrributes:
+           size (int): this represents the size attribute of the Square.
+           x (int): this represent the x-cordinate of the Square object.
+           y (int): this represents the y-cordinate of the Square object.
+           id (int): this reperents the id of the Square object.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.__size = size
+
+    def __str__(self):
+        idx = self.id
+        x_c = self.x
+        y_c = self.y
+        rep = "[Sqaure] ({}) {}/{} - {}".format(idx, x_c, y_c, self.__size)
+        return rep
