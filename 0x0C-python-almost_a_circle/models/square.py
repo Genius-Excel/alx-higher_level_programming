2914516fdb3ad13196954b9ff9fce368d739fa60
Genius-Excel <geniusexcel9@gmail.com>
2023-12-09 20:31:45 +0100

    A new public method 'update' was implemented.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,8 +33,28 @@ class Square(Rectangle):
         return self.__width
 
     @Rectangle.width.setter
-    #@size.setter
+    # @size.setter
     def size(self, value):
         self.__width = value
         # Rectangle.width.fset(self, value)
         # self.height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+
+        if not args:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.__size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
