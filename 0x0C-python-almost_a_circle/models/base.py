cfe1aa46b0e55242ab477c51d25fe72b61fef25d
Genius-Excel <geniusexcel9@gmail.com>
2023-12-10 14:29:06 +0100

    File modified, classmethod now produce expeced output.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,13 +26,19 @@ class Base(object):
     def to_json_string(list_dictionaries):
         if not list_dictionaries:
             return "[]"
-        
+
         return json.dumps(list_dictionaries)
+
+    def change_to_dictionary(self, data_obj):
+        pass
 
     @classmethod
     def save_to_file(cls, list_objs):
-        for obj in list_objs:
-            cls.filename = "{}.json".format(obj.__class__.__name__)
+        cls.json_obj_list = []
+        cls.filename = "{}.json".format(cls.__name__)
+
+        for cls_obj in list_objs:
+            cls.json_obj_list.append(cls_obj.to_dictionary())
 
         with open(cls.filename, "w") as file:
-            file.write(cls.to_json_string(list_objs))
+            file.write(cls.to_json_string(cls.json_obj_list))
