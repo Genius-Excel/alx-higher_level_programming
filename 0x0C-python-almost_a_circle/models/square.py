42cbf0db30715c622efc3bb8fac095c360c4e22e
Genius-Excel <geniusexcel9@gmail.com>
2023-12-11 17:08:23 -0800

    file modified.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,16 +43,16 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """update method"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.__size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) >= 4:
-            self.y = args[3]
-
-        if not args:
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
             if 'id' in kwargs:
                 self.id = kwargs['id']
             if 'size' in kwargs:
