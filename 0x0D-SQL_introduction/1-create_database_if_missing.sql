0834a48e4a13ad35466aae8289fb6827a972d8e1
Genius-Excel <geniusexcel9@gmail.com>
2024-01-16 14:33:22 -0800

    This script creates a new Relationd database, if it exists; it uses the databse without failing else it creates the databse should it not be present.

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,14 @@
+-- This SQL script contins a query that creates a database if does not exist.
+-- Also it avoid faures sould the database exist.
+
+USE mysql;
+SET @new_db_name := 'hbtn_0c_0';
+SET @db_present := (SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = @new_db_name);
+
+-- Create database Dynamically without failing.
+SET @sql_query := IF(@db_present = 0, CONCAT('CREATE DATABASE ', @new_db_name), '');
+PREPARE create_stmt FROM @sql_query;
+EXECUTE create_stmt;
+DEALLOCATE PREPARE create_stmt;
+
+USE hbtn_0c_0;
