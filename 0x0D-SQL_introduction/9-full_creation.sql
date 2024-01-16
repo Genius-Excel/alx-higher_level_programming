97f4c7303f9bdf21c682d1a1fa53164968217ea7
Genius-Excel <geniusexcel9@gmail.com>
2024-01-16 15:42:59 -0800

    This script performs a full creation of a table.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,3 @@
 -- This sql script performs a full creation of a databse table and populates it with data.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (id = 1, name = "John", score = 10);
-INSERT INTO second_table (id, name, score) VALUES (id = 2, name = "Alex", score = 3);
-INSERT INTO second_table (id, name, score) VALUES (id = 3, name = "Bob", score = 14);
-INSERT INTO second_table (id, name, score) VALUES (id = 4, name = "George", score = 8);
+INSERT INTO second_table (id, name, score) VALUES (1,"John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
