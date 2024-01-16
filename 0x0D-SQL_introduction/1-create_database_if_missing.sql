686e65f8986caf0ab95d558e4c0f7175a1a3e8d7
Genius-Excel <geniusexcel9@gmail.com>
2024-01-16 14:38:31 -0800

    This sql script creates a new databse if does not exist without failing.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,11 +1,3 @@
 -- This SQL script contins a query that creates a database if does not exist.
 
-USE mysql;
-SET @new_db_name := 'hbtn_0c_0';
-SET @db_present := (SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = @new_db_name);
-SET @sql_query := IF(@db_present = 0, CONCAT('CREATE DATABASE ', @new_db_name), '');
-PREPARE create_stmt FROM @sql_query;
-EXECUTE create_stmt;
-DEALLOCATE PREPARE create_stmt;
-
-USE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
