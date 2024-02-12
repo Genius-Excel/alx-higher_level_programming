a086b97d550f457b9d52dcfb8b7af6b56e26601d
Genius-Excel <geniusexcel9@gmail.com>
2024-02-12 19:11:24 +0100

    My language preference.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const iLoveC = 'C is fun';
+const iLovePythonMore = 'Python is cool';
+const iLikeJs = 'JavaScript is amazing';
+
+console.log(iLoveC);
+console.log(iLovePythonMore);
+console.log(iLikeJs);
