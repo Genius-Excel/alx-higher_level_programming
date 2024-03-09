ce7db60d13616148c3c3f11f6a8c37414622a2d6
Genius-Excel <geniusxecl9@gmail.com>
2024-03-09 05:48:28 -0800

    This SQL script creates a new user identified by a password in a mysql server and grants all privileges.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- this script ceates a new user in a mysql server without failing.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
