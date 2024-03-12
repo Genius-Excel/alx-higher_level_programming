852136c1a8da99e736316e2a7389cf4a7563ec0b
Genius-Excel <geniusxecl9@gmail.com>
2024-03-12 14:38:53 -0700

    Error resolved.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- this sql script cheks for a user's priviledge on a mysql server .
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localchost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
